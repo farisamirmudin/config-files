195bc14b9a6396a95958814fedbfaeb852163688
faris <faris071298@gmail.com>
2025-05-25 22:46:09 +0800

    launch wezterm maximized on startup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,17 @@
 local wezterm = require('wezterm')
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 local config = {
 	automatically_reload_config = true,
 	enable_tab_bar = false,
 	window_decorations = 'RESIZE',
 	color_scheme = 'tokyonight',
-	window_background_opacity = 0.8
+	window_background_opacity = 0.90
 }
 
 return config

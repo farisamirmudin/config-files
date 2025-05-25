ac4be818d6363d659fa42a804c0623756efd09ac
Faris Amirmudin <farisamirmudin@rolexwatch.local>
2025-05-25 20:58:30 +0800

    add fking starship and wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require('wezterm')
+
+local config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_decorations = 'RESIZE',
+	color_scheme = 'tokyonight',
+	window_background_opacity = 0.8
+}
+
+return config

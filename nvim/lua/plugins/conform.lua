aa1a42a8a4f46b08b14d43423658b25342c83374
faris <faris071298@gmail.com>
2025-05-26 22:40:00 +0800

    add ts to conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
 				formatters_by_ft = {
 					lua = { "stylua" },
 					javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+					typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
 				},
 				format_on_save = {
 					timeout_ms = 500,

f35127fcbf2c8a929d28dcfab64cd95198b13c0c
faris <faris071298@gmail.com>
2025-05-26 22:21:57 +0800

    use tmux to handle tab and split

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,8 +8,8 @@ end)
 
 local config = {
 	automatically_reload_config = true,
-	enable_tab_bar = false,
-	window_decorations = 'RESIZE',
+	-- enable_tab_bar = false,
+	-- window_decorations = 'RESIZE',
 	color_scheme = 'tokyonight',
 	window_background_opacity = 0.90
 }

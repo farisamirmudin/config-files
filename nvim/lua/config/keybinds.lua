f8c34c7a2f9ed673c452614ebc6f1b7aa02338f3
faris <faris071298@gmail.com>
2025-05-31 21:21:50 +0800

    update split command

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>tp', "<Esc>:!tmux split-window -h 'nvim %'<CR>")
+
+vim.keymap.set('n', '<leader>tp', function()
+	local file = vim.fn.expand('%:p')
+	vim.system({ 'tmux', 'split-window', '-h', 'nvim', file })
+end, { desc = 'Open current file in vertical tmux split' })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,4 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.autoread = true
